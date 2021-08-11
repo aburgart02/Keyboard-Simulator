7f182044162e4b02a5b5668988f7f6c22fa7227d
artem <a.burgart02@gmail.com>
2021-08-11 17:40:36 +0500

    added random text option

--- text_selection.py
+++ text_selection.py
@@ -1,4 +1,5 @@
 import settings
+import random
 from PyQt5.QtWidgets import QWidget, QPushButton, QFileDialog
 from functools import partial
 
@@ -9,7 +10,6 @@ class TextSelection(QWidget):
         self.next_window = False
         self.previous_window = False
         self.toggle_full_screen = False
-        self.my_text_button = QPushButton("Выбрать свой текст", self)
         self.initialize_rus_texts()
         self.initialize_eng_texts()
         self.assign_buttons()
@@ -35,10 +35,12 @@ class TextSelection(QWidget):
         self.rus_text_1 = QPushButton("100 Символов", self)
         self.rus_text_2 = QPushButton("200 Символов", self)
         self.rus_text_3 = QPushButton("300 Символов", self)
+        self.rus_random_text = QPushButton("Случайный текст", self)
+        self.rus_my_text = QPushButton("Выбрать свой текст", self)
         self.rus_texts = [self.rus_lesson_1, self.rus_lesson_2, self.rus_lesson_3, self.rus_lesson_4,
                           self.rus_lesson_5, self.rus_lesson_6, self.rus_lesson_7, self.rus_lesson_8,
                           self.rus_lesson_9, self.rus_lesson_10, self.rus_text_1, self.rus_text_2,
-                          self.rus_text_3, self.my_text_button]
+                          self.rus_text_3, self.rus_random_text, self.rus_my_text]
 
     def initialize_eng_texts(self):
         self.eng_lesson_1 = QPushButton("Lesson 1", self)
@@ -54,10 +56,12 @@ class TextSelection(QWidget):
         self.eng_text_1 = QPushButton("100 Characters", self)
         self.eng_text_2 = QPushButton("200 Characters", self)
         self.eng_text_3 = QPushButton("300 Characters", self)
+        self.eng_random_text = QPushButton("Random text", self)
+        self.eng_my_text = QPushButton("Choose your own text", self)
         self.eng_texts = [self.eng_lesson_1, self.eng_lesson_2, self.eng_lesson_3, self.eng_lesson_4,
                           self.eng_lesson_5, self.eng_lesson_6, self.eng_lesson_7, self.eng_lesson_8,
                           self.eng_lesson_9, self.eng_lesson_10, self.eng_text_1, self.eng_text_2,
-                          self.eng_text_3, self.my_text_button]
+                          self.eng_text_3, self.eng_random_text, self.eng_my_text]
 
     def configure_elements(self, ratio):
         for buttons in [self.rus_texts, self.eng_texts]:
@@ -75,11 +79,14 @@ class TextSelection(QWidget):
                 row += 1
 
     def assign_buttons(self):
-        for k in range(len(self.rus_texts) - 1):
+        for k in range(len(self.rus_texts) - 2):
             self.rus_texts[k].clicked.connect(partial(self.set_text, r'texts\rus_texts\t' + str(k) + '.txt'))
-        for k in range(len(self.eng_texts) - 1):
+        for k in range(len(self.eng_texts) - 2):
             self.eng_texts[k].clicked.connect(partial(self.set_text, r'texts\eng_texts\t' + str(k) + '.txt'))
-        self.my_text_button.clicked.connect(self.get_text_file)
+        self.rus_my_text.clicked.connect(self.get_text_file)
+        self.eng_my_text.clicked.connect(self.get_text_file)
+        self.rus_random_text.clicked.connect(lambda x: self.create_text(r'texts\rus_texts\rus_words.txt'))
+        self.eng_random_text.clicked.connect(lambda x: self.create_text(r'texts\eng_texts\eng_words.txt'))
 
     def change_resolution(self, ratio):
         self.setFixedSize(1280 * ratio, 720 * ratio)
@@ -87,11 +94,20 @@ class TextSelection(QWidget):
 
     def set_rus_texts(self):
         [text.show() for text in self.rus_texts]
-        [text.hide() for text in self.eng_texts[:-1]]
+        [text.hide() for text in self.eng_texts]
 
     def set_eng_texts(self):
         [text.show() for text in self.eng_texts]
-        [text.hide() for text in self.rus_texts[:-1]]
+        [text.hide() for text in self.rus_texts]
+
+    def create_text(self, path):
+        text = ''
+        with open(path, 'r', encoding='utf-8') as f:
+            words = f.read().split('\n')
+        for _ in range(0, 30):
+            text += words[random.randint(0, len(words) - 1)] + ' '
+        settings.text = text.strip()
+        self.next_window = True
 
     def get_text_file(self):
         path = QFileDialog.getOpenFileName()[0]
